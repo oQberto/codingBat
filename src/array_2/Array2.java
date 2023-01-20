bb070f71f4c5d84198db108d513573a461a287bb
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-20 17:29:03 +0300

    Add "lucky13" method
    Given an array of ints, return true if the array contains no 1's and no 3's.
    
    lucky13([0, 2, 4]) → true
    lucky13([1, 2, 3]) → false
    lucky13([1, 2, 4]) → false

--- src/array_2/Array2.java
+++ src/array_2/Array2.java
@@ -2,6 +2,17 @@ package array_2;
 
 public class Array2 {
 
+    public static boolean lucky13(int[] array) {
+        boolean isLucky = true;
+        for (int element : array) {
+            if (element == 1 || element == 3) {
+                isLucky = false;
+                break;
+            }
+        }
+        return isLucky;
+    }
+
     public static int sum13(int[] array) {
         if (array == null) {
             return 0;
