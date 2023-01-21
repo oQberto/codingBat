fb77ed45b5075eab4ab466e60aad5e7e1e9b79b2
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-21 17:21:02 +0300

    Add "sum67" method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import com.codingbat.tasks.array_2.Array2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Array2.bigDif(new int[]{}));
+        System.out.println(Array2.sum67(new int[]{1, 1, 6, 7, 2}));
     }
 }
